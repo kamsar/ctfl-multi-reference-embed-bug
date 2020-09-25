cfbd90b5549de8b55ca04f60e2489e27396a35d8
Kam Figy <kam.figy@uniform.dev>
2020-09-25 07:29:57 -0700

    bug repro

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
-import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import React from "react";
+import { FieldExtensionSDK } from "contentful-ui-extensions-sdk";
+import { MultipleEntryReferenceEditor } from "@contentful/field-editor-reference";
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (prop: FieldProps) => {
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  prop.sdk.window.startAutoResizer();
+  return (
+    <MultipleEntryReferenceEditor
+      sdk={prop.sdk}
+      isInitiallyDisabled={false}
+      viewType="link"
+      parameters={{
+        instance: {
+          showCreateEntityAction: true,
+          showLinkEntityAction: true
+        },
+      }}
+      hasCardEditActions={true}
+    />
+  );
 };
 
 export default Field;
